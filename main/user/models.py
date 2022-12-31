fee5aca111be6dbf02fb486e595b113574e6d854
lecrowpus <95230960+lecrowpus@users.noreply.github.com>
2022-12-31 20:51:59 +0530

    Delete main directory

--- main/user/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class users(models.Model):
-    username=models.CharField(max_length=20)
-    password=models.CharField(max_length=200)
-    email=models.EmailField()
-    state=models.BooleanField()
